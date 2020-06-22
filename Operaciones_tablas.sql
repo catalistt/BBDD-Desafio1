4ed2c1b3f07e7da802fadf99abd1ab6830516637
Catalina <caliste@fen.uchile.cl>
2020-06-22 00:03:49 -0400

    Errores arreglados

--- Operaciones_tablas.sql
+++ Operaciones_tablas.sql
@@ -2,56 +2,56 @@ CREATE DATABASE posts;
 \c posts;
 
 CREATE TABLE posts(
-    id VARCHAR(5),
+    id_post SERIAL,
     nombre_usuario VARCHAR(25),
     fecha_creacion DATE,
     contenido VARCHAR(1000),
     descripcion VARCHAR(1000),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id_post)
 );
 
-INSERT INTO posts(id,nombre_usuario,fecha_creacion, contenido, descripcion) VALUES ('P01','Pamela', '2020-06-15', 'este es un post1', 'post 1 Pamela');
-INSERT INTO posts(id,nombre_usuario,fecha_creacion, contenido, descripcion) VALUES ('P02','Pamela', '2020-06-15', 'este es un post2', 'post 2 Pamela');
-INSERT INTO posts(id,nombre_usuario,fecha_creacion, contenido, descripcion) VALUES ('P03','Carlos', '2020-06-15', 'este es un post3', 'post 1 Carlos');
+INSERT INTO posts(nombre_usuario,fecha_creacion, contenido, descripcion) VALUES ('Pamela', '2020-06-15', 'este es un post1', 'post 1 Pamela');
+INSERT INTO posts(nombre_usuario,fecha_creacion, contenido, descripcion) VALUES ('Pamela', '2020-06-15', 'este es un post2', 'post 2 Pamela');
+INSERT INTO posts(nombre_usuario,fecha_creacion, contenido, descripcion) VALUES ('Carlos', '2020-06-15', 'este es un post3', 'post 1 Carlos');
 
 ALTER TABLE posts ADD titulo VARCHAR(15);
 
 UPDATE posts
 SET titulo = 'post de Pamela'
-WHERE nombre_usuario = 'Pamela';
+WHERE id_post = 1 or id_post = 2;
 
 UPDATE posts
 SET titulo = 'post de Carlos'
-WHERE nombre_usuario = 'Carlos';
+WHERE id_post = 3;
 
-INSERT INTO posts(id,nombre_usuario,fecha_creacion, contenido, descripcion, titulo) VALUES ('P04','Pedro', '2020-06-15', 'este es un post4', 'post 1 Pedro', 'post de Pedro');
-INSERT INTO posts(id,nombre_usuario,fecha_creacion, contenido, descripcion, titulo) VALUES ('P05','Pedro', '2020-06-15', 'este es un post5', 'post 2 Pedro', 'post de Pedro');
+INSERT INTO posts(nombre_usuario,fecha_creacion, contenido, descripcion, titulo) VALUES ('Pedro', '2020-06-15', 'este es un post4', 'post 1 Pedro', 'post de Pedro');
+INSERT INTO posts(nombre_usuario,fecha_creacion, contenido, descripcion, titulo) VALUES ('Pedro', '2020-06-15', 'este es un post5', 'post 2 Pedro', 'post de Pedro');
 
 DELETE FROM posts
-WHERE nombre_usuario = 'Carlos';
+WHERE id_post = 3;
 
-INSERT INTO posts(id,nombre_usuario,fecha_creacion, contenido, descripcion) VALUES ('P06','Carlos', '2020-06-16', 'este es un nuevo post6', 'post nuevo Carlos');
-
+INSERT INTO posts(nombre_usuario,fecha_creacion, contenido, descripcion, titulo) VALUES ('Carlos', '2020-06-16', 'este es un nuevo post6', 'post nuevo Carlos', 'post de Carlos');
 
 CREATE TABLE comentarios(
-    id VARCHAR(5),
+    id_comentarios SERIAL,
     fecha_hora_creacion TIMESTAMP,
     contenido VARCHAR(1000),
-    FOREIGN KEY (id) REFERENCES
-    posts(id)    
+    post_relacionado INT,
+    FOREIGN KEY (post_relacionado) REFERENCES
+    posts(id_post)    
     );
 
-INSERT INTO comentarios(id, fecha_hora_creacion, contenido) VALUES ('P01','21/12/2019 13:00:00.59','Primer comentario');
-INSERT INTO comentarios(id, fecha_hora_creacion, contenido) VALUES ('P01','21/12/2019 14:00:00','Segundo comentario');
+INSERT INTO comentarios(fecha_hora_creacion, contenido, post_relacionado) VALUES ('21/12/2019 13:00:00.59','Primer comentario pamela',1);
+INSERT INTO comentarios(fecha_hora_creacion, contenido, post_relacionado) VALUES ('21/12/2019 14:00:00','Segundo comentario pamela',1);
 
-INSERT INTO comentarios(id, fecha_hora_creacion, contenido) VALUES ('P06','10/06/2020 14:00:00','Primer comentario');
-INSERT INTO comentarios(id, fecha_hora_creacion, contenido) VALUES ('P06','11/06/2020 14:00:00','Segundo comentario');
-INSERT INTO comentarios(id, fecha_hora_creacion, contenido) VALUES ('P06','12/06/2020 14:00:00','Tercer comentario');
-INSERT INTO comentarios(id, fecha_hora_creacion, contenido) VALUES ('P06','13/06/2020 14:00:00','Cuarto comentario');
+INSERT INTO comentarios(fecha_hora_creacion, contenido, post_relacionado) VALUES ('10/06/2020 14:00:00','Primer comentario',6);
+INSERT INTO comentarios(fecha_hora_creacion, contenido, post_relacionado) VALUES ('11/06/2020 14:00:00','Segundo comentario',6);
+INSERT INTO comentarios(fecha_hora_creacion, contenido, post_relacionado) VALUES ('12/06/2020 14:00:00','Tercer comentario',6);
+INSERT INTO comentarios(fecha_hora_creacion, contenido, post_relacionado) VALUES ('13/06/2020 14:00:00','Cuarto comentario',6);
 
-INSERT INTO posts(id,nombre_usuario,fecha_creacion, contenido, descripcion) VALUES ('P07','Margarita', '2020-06-16', 'este es un post', 'post de Margarita');
+INSERT INTO posts(nombre_usuario,fecha_creacion, contenido, descripcion, titulo) VALUES ('Margarita', '2020-06-16', 'este es un post', 'post de Margarita','post M');
 
-INSERT INTO comentarios(id, fecha_hora_creacion, contenido) VALUES ('P07','16/06/2020 14:00:00','Primer comentario');
-INSERT INTO comentarios(id, fecha_hora_creacion, contenido) VALUES ('P07','16/06/2020 14:00:00','Segundo comentario');
-INSERT INTO comentarios(id, fecha_hora_creacion, contenido) VALUES ('P07','16/06/2020 14:00:00','Tercer comentario');
-INSERT INTO comentarios(id, fecha_hora_creacion, contenido) VALUES ('P07','16/06/2020 14:00:00','Cuarto comentario');
+INSERT INTO comentarios(fecha_hora_creacion, contenido, post_relacionado) VALUES ('16/06/2020 14:00:00','Primer comentario',7);
+INSERT INTO comentarios(fecha_hora_creacion, contenido, post_relacionado) VALUES ('16/06/2020 14:00:00','Segundo comentario',7);
+INSERT INTO comentarios(fecha_hora_creacion, contenido, post_relacionado) VALUES ('16/06/2020 14:00:00','Tercer comentario',7);
+INSERT INTO comentarios(fecha_hora_creacion, contenido, post_relacionado) VALUES ('16/06/2020 14:00:00','Cuarto comentario',7);
